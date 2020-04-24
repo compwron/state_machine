e16e64dc8362b56b43d64fae1219e1238fee750d
Linda <linda@semprehealth.com>
2020-04-24 00:01:53 -0700

    WIP rake appraisal:all failing on uninitialized constant Test (NameError)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,6 @@ if ENV['COVERAGE']
   SimpleCov.start { add_filter '/test/' }
 end
 
-require 'test/unit'
+# require 'test/unit'
+Dir["/test/unit/*.rb"].each {|file| require file }
 require 'state_machine'
